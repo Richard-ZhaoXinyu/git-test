02c7cfd7487b6700d68dbf50d97537c161f4b620
Richard <Richard.163.com>
2022-03-16 21:47:43 +0800

    first commit

---
+++ src/main/java/com/zxy/git/Test01.java
@@ -0,0 +1,14 @@
+package com.zxy.git;
+
+/**
+ * Description:
+ *
+ * @author Richard
+ * @date Created on 2022/3/16
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
