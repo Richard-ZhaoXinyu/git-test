876503cec4893698d560ddbb29385f60713d7a00
Richard <Richard.163.com>
2022-03-16 22:24:04 +0800

    hotFix commit02

--- src/main/java/com/zxy/git/Test01.java
+++ src/main/java/com/zxy/git/Test01.java
@@ -13,5 +13,6 @@ public class Test01 {
         System.out.println("hello,git2!");
         System.out.println("hello,git3!");
         System.out.println("hello,git4!");
+        System.out.println("hello,world");
     }
 }
